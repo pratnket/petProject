72fdf32fc073f4b89c10fe62da82b93a1648e9e3
pratnket <m22301162@gmail.com>
2025-09-11 19:51:39 +0800

    全頁面 添加裝置安全區域 fix

--- src/screens/HotelDetailScreen.tsx
+++ src/screens/HotelDetailScreen.tsx
@@ -3,10 +3,11 @@ import PageWrapper from '../components/common/PageWrapper';
 import HotelDetail from '../components/common/HotelDetail';
 
 const HotelDetailScreen = props => {
-  return 
-  <PageWrapper>
-    <HotelDetail {...props} />
-  </PageWrapper>;
+  return (
+    <PageWrapper>
+      <HotelDetail {...props} />
+    </PageWrapper>
+  );
 };
 
 export default HotelDetailScreen;
